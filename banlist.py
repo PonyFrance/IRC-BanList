7e76d5f1f7507ecf594c597275ba6dbdb64d03ff
Kody <im@kdy.ch>
2016-05-17 10:18:47 +0200

    Update banlist.py

--- banlist.py
+++ banlist.py
@@ -1,8 +1,8 @@
 import time
 with open("banlist.txt", "r") as file:
 	print("*Generated `" + time.ctime() + " (" + time.tzname[1] + ")`*")
-	print("\n| Date | Host | By | Notes |")
-	print("|------|------|----|-------|")
+	print("\n| Date | Host | Notes |")
+	print("|------|------|-------|")
 	for line in file:
 		split = line.split()
-		print("| " + time.ctime(int(split[-1])) + " | `" + split[-3] + "` | " + split[-2] + " |   |")
+		print("| " + time.ctime(int(split[-1])) + " | `" + split[-3] + "` |  |")
